c61b4cc74ff75fa37d618a90300e461504bb1769
ABHINAV RAJHANS <68992789+Abhinavrajhans@users.noreply.github.com>
2021-04-14 17:47:18 +0530

    small update in additems

--- src/pages/additems.js
+++ src/pages/additems.js
@@ -10,56 +10,74 @@ const AddItems = () => {
           <img src={close} alt="" />
         </span>
       </Heading12>
-      <div>
-        <form action="">
-          <div>
-            <Forming>
-              <div>
-                <label>Type</label>
-              </div>
-              <Radios>
-                <Rady>
-                  <input type="radio" name="Itemsmap" id="Goods"></input>
-                  <label for="Goods">Goods</label>
-                </Rady>
-                <Rady>
-                  <input type="radio" name="Itemsmap" id="Service"></input>
-                  <label for="Service">Service</label>
-                </Rady>
-              </Radios>
-            </Forming>
-          </div>
-          <div>
-            <label>Name*</label>
-            <input></input>
-          </div>
-          <div>
-            <label>SKU</label>
-            <input></input>
-          </div>
-          <div>
-            <label>Unit*</label>
-            <select>
-              <option value="box">box</option>
-              <option value="cm">cm</option>
-              <option value="dz">dz</option>
-              <option value="ft">ft</option>
-              <option value="g">g</option>
-              <option value="in">in</option>
-              <option value="kg">kg</option>
-              <option value="km">km</option>
-              <option value="lb">lb</option>
-              <option value="mg">mg</option>
-              <option value="m">m</option>
-              <option value="pcs">pcs</option>
-            </select>
-          </div>
-        </form>
-      </div>
+      <Flexer>
+        <div class="fill">
+          <form action="">
+            <div>
+              <Forming>
+                <Naming12>
+                  <label>Type</label>
+                </Naming12>
+                <Radios>
+                  <Rady>
+                    <input type="radio" name="item" id="Goods"></input>
+                    <label for="Goods">Goods</label>
+                    <input type="radio" name="item" id="Service"></input>
+                    <label for="Service">Service</label>
+                  </Rady>
+                </Radios>
+              </Forming>
+            </div>
+            <Naming>
+              <label>Name*</label>
+              <Bounce>
+                <input></input>
+              </Bounce>
+            </Naming>
+            <Naming>
+              <label>SKU</label>
+              <Bounce>
+                <input></input>
+              </Bounce>
+            </Naming>
+            <Naming>
+              <label>Unit*</label>
+              <Bounce>
+                <select>
+                  <option value="box">box</option>
+                  <option value="cm">cm</option>
+                  <option value="dz">dz</option>
+                  <option value="ft">ft</option>
+                  <option value="g">g</option>
+                  <option value="in">in</option>
+                  <option value="kg">kg</option>
+                  <option value="km">km</option>
+                  <option value="lb">lb</option>
+                  <option value="mg">mg</option>
+                  <option value="m">m</option>
+                  <option value="pcs">pcs</option>
+                </select>
+              </Bounce>
+            </Naming>
+          </form>
+        </div>
+        <div className="picker">
+          <label for="myfile">Select a file:</label>
+          <input type="file" id="myfile" name="myfile"></input>
+        </div>
+      </Flexer>
     </ItemMaking>
   );
 };
-const ItemMaking = styled.div``;
+
+const ItemMaking = styled.div`
+  label {
+    font-size: 13px;
+
+    width: 100%;
+    height: 100%;
+  }
+`;
 const Heading12 = styled.div`
   display: flex;
   flex-direction: row;
@@ -81,9 +99,55 @@ const Radios = styled.form`
 `;
 const Forming = styled.form`
   display: flex;
+  flex-direction: row !important;
+  justify-content: space-between;
 `;
 const Rady = styled.form`
   margin: 0px 9.75px 0px 0px;
   display: flex;
+  flex-direction: row !important;
+  gap: 10px;
+`;
+const Naming = styled.div`
+  display: flex;
+  flex-direction: row;
+  label {
+    padding: 5.875px 15px;
+  }
+  input {
+    padding: 5px 8px;
+  }
+  select {
+    padding: 5px 30px 5px 8px;
+  }
+`;
+const Naming12 = styled.div`
+  label {
+    padding: 0px 15px 5.875px;
+  }
+`;
+const Bounce = styled.div`
+  padding: 0px 15px;
+  width: 100%;
+  select {
+    width: 100%;
+  }
+`;
+const Flexer = styled.div`
+  display: grid;
+  .picker {
+    justify-items: center;
+    grid-area: b1;
+  }
+  .fill {
+    padding: 0px 15px;
+    grid-area: b2;
+  }
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+  }
+  grid-template-areas: "b2 . . b1";
 `;
 export default AddItems;
